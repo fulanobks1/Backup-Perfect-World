a134e9b425f395bb4a6a37ae8ad4ba714f092f0f
fulanobks1 <47329501+fulanobks1@users.noreply.github.com>
2020-10-02 03:12:28 -0300

    Create extrair.sh

---
+++ extrair.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+data=$(date "+%F")
+
+##################################################################
+
+nome=New History
+ip=IP_DA_SUA_MAQUINA
+
+##################################################################
+
+echo "Iniciando o salvamento do Backup"
+echo "Backup $nome - Data: $data"
+echo ""
+##################################################################
+
+echo "Baixando o Backup"
+cd /Backup/
+
+wget http://$ip:80/totalbackup/$data-mysql.tar.bz2
+wget http://$ip:80/totalbackup/$data-logs.tar.bz2
+wget http://$ip:80/totalbackup/$data.tar.bz2
+
+echo "Backup baixado com Sucesso!"
